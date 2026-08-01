b7942d5119005f79f07f6b95e9cae2e6ac6d8447
srce4200 <tjaz.saksida@gmail.com>
2026-08-01 15:07:49 +0200

    Terminal parameters detection

--- 2_ExpenseTracker.cpp
+++ 2_ExpenseTracker.cpp
@@ -1,5 +1,58 @@
 #include <iostream>
+#include <ctime>
+#include <string>
 
-int main(){
+class Expense{
+    std::string description;
+    double amaunt;
+    int IDs;
+    time_t date;
+
+    public:
+        Expense(std::string d,  int am, time_t t){
+            this->amaunt = am;
+            this->description = d;
+            this->date = t;
+        }    
+        void LogStats(){
+            std::cout<< "\n# " << IDs << "\t" << date << "\t" << description << "\t" << amaunt;
+        }
+};
+
+void add(std::string description, int amaunt);
+void summary();
+
+
+//add list summary delete
+int main(int argc, char *argv[]){
+    std::string targetFnct = argv[1];
+    if(targetFnct.compare("add")==0){
+
+        add(argv[2], std::stoi(argv[3]));
+    }
+    else if(targetFnct.compare("summary")==0){
+
+    }
+    else if(targetFnct.compare("delete")==0){
+
+    }
+    else if(targetFnct.compare("list")==0){
+
+    }
+    else{
+        std::cout << "Invalid command.";
+    }
+
     return 0;
+}
+
+void add(std::string desc, int amaunt){
+    time_t curTime;
+    Expense exp(desc, amaunt, time(&curTime));
+    exp.LogStats();
+    std::cout << "\n# Expense added successfully";
+}
+void list(){
+    std::cout << "\n# ID  Date       Description  Amount";
+
 }
